c6c9fe6221a62b565f198e6d2de25a3289d2f5e8
Valentin Vieriu <vvieriu@sapient.com>
2017-06-13 10:20:56 +0200

    rebased on tracking master

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,17 +3,16 @@ export default {
   // current list type and current pagination
   activeIds (state) {
     const { activeType, itemsPerPage, lists, activePage } = state
-    const page = activePage[activeType] || Number(state.route.params.page) || 1
 
     if (!activeType) {
       return []
     }
 
-    const page = Number(state.route.params.page) || 1
+    const page = activePage[activeType] || Number(state.route.params.page) || 1
     const start = (page - 1) * itemsPerPage
     const end = page * itemsPerPage
 
-    return lists[activeType].slice(start, end)
+    return lists[activeType].slice(0, end)
   },
 
   // items that should be currently displayed.
