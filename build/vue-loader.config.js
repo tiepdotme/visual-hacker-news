408d49ee83113ffaa23e85d6af5a02bf5b5dd304
Valentin Vieriu <vvieriu@sapient.com>
2017-07-23 09:33:17 +0200

    disabled extraction of css to obtain higher lighthouse score

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extractCSS: process.env.NODE_ENV === 'production',
+  extractCSS: false , //process.env.NODE_ENV === 'production',
   preserveWhitespace: false,
   postcss: [
     require('autoprefixer')({
